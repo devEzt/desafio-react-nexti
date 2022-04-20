50078520e4a530fe997adbed42468d4dc4ac946a
AlexandreDev <eztalenda@gmail.com>
2022-04-20 00:23:59 -0300

    feat(pages): add page authentication

--- src/Providers/index.js
+++ src/Providers/index.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 import i18n from './i18n'
 
 import { I18nextProvider } from 'react-i18next'
 
+import Auth from './auth'
+
+import { ReactQueryProvider } from './reactQuery'
+
 const Providers = ({ children }) => {
-  return <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+  return (
+    <I18nextProvider i18n={i18n}>
+      <ReactQueryProvider>
+        <Suspense fallback={() => <p>Carregando...</p>}>
+          <Auth>{children}</Auth>
+        </Suspense>
+      </ReactQueryProvider>
+    </I18nextProvider>
+  )
 }
 
 export default Providers
